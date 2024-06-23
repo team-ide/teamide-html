41c40fe41915f3297e7bdc08b2a688265d847ba4
朱亮 <teamide@163.com>
2024-06-23 18:09:13 +0800

    commit

--- src/component/flowchart/Container.vue
+++ src/component/flowchart/Container.vue
@@ -43,6 +43,225 @@ export default {
           },
         },
       });
+
+      this.tool.jQuery(this.$el).append(
+        this.createShapeCanvas({
+          left: 100,
+          top: 100,
+          width: 100,
+          height: 50,
+          type: "rect",
+          bWidth: 6,
+          bColor: "#fff",
+        })
+      );
+
+      this.tool.jQuery(this.$el).append(
+        this.createShapeCanvas({
+          left: 250,
+          top: 100,
+          width: 100,
+          height: 50,
+          type: "rect",
+          bgColor: "red",
+          bWidth: 6,
+          bColor: "#fff",
+        })
+      );
+
+      this.tool.jQuery(this.$el).append(
+        this.createShapeCanvas({
+          left: 100,
+          top: 200,
+          width: 100,
+          height: 100,
+          type: "circle",
+          bWidth: 6,
+          bColor: "#fff",
+        })
+      );
+
+      this.tool.jQuery(this.$el).append(
+        this.createShapeCanvas({
+          left: 250,
+          top: 200,
+          width: 100,
+          height: 100,
+          type: "circle",
+          bgColor: "red",
+          bWidth: 6,
+          bColor: "#fff",
+        })
+      );
+
+      this.tool.jQuery(this.$el).append(
+        this.createShapeCanvas({
+          left: 100,
+          top: 350,
+          width: 100,
+          height: 50,
+          type: "rect-circle",
+          bWidth: 6,
+          bColor: "#fff",
+        })
+      );
+      this.tool.jQuery(this.$el).append(
+        this.createShapeCanvas({
+          left: 250,
+          top: 350,
+          width: 100,
+          height: 50,
+          type: "rect-circle",
+          bgColor: "red",
+          bWidth: 6,
+          bColor: "#fff",
+        })
+      );
+
+      this.tool.jQuery(this.$el).append(
+        this.createShapeCanvas({
+          left: 100,
+          top: 500,
+          width: 100,
+          height: 50,
+          type: "parallelogram",
+          bWidth: 6,
+          bColor: "#fff",
+        })
+      );
+      this.tool.jQuery(this.$el).append(
+        this.createShapeCanvas({
+          left: 250,
+          top: 500,
+          width: 100,
+          height: 50,
+          type: "parallelogram",
+          bgColor: "red",
+          bWidth: 6,
+          bColor: "#fff",
+        })
+      );
+    },
+    createShapeCanvas(options) {
+      let el = this.tool.jQuery(`<div class="flowchart-new-shape"></div>`);
+      el.css({
+        left: options.left,
+        top: options.top,
+        width: options.width,
+        height: options.height,
+      });
+
+      el.append(this.createCanvas(options));
+      return el;
+    },
+    createCanvas(options) {
+      let w = options.width;
+      let h = options.height;
+      let canvas = this.tool.jQuery(
+        `<canvas class="flowchart-canvas" width="${w}" height="${h}"></canvas>`
+      );
+      this.draw(canvas[0], options);
+      return canvas[0];
+    },
+    draw(canvas, options) {
+      let w = options.width;
+      let h = options.height;
+      let bW = Number(options.bWidth || 1);
+      let bColor = options.bColor || "#ddd";
+      let bgColor = options.bgColor;
+
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, w, h);
+      // 矩形
+      if (options.type == "rect") {
+        if (this.tool.isNotEmpty(bgColor)) {
+          ctx.beginPath();
+          ctx.fillStyle = bColor;
+          ctx.roundRect(0, 0, w, h, 6);
+          ctx.fill();
+
+          ctx.beginPath();
+          ctx.fillStyle = bgColor;
+          ctx.roundRect(bW, bW, w - bW * 2, h - bW * 2, 3);
+          ctx.fill();
+        } else {
+          ctx.lineWidth = bW;
+          ctx.beginPath();
+          ctx.strokeStyle = bColor;
+          ctx.roundRect(bW / 2, bW / 2, w - bW, h - bW, 3);
+          ctx.stroke();
+        }
+      }
+      // 圆形
+      else if (options.type == "circle") {
+        let x = w / 2;
+        let y = h / 2;
+        if (this.tool.isNotEmpty(bgColor)) {
+          ctx.beginPath();
+          ctx.fillStyle = bColor;
+          // 圆心x、圆心y、圆的半径、圆弧的开始角度、圆弧的结束角度、是否逆时针
+          ctx.arc(x, y, x, 0, (360 * Math.PI) / 180, false);
+          ctx.fill();
+
+          ctx.beginPath();
+          ctx.fillStyle = bgColor;
+          // 圆心x、圆心y、圆的半径、圆弧的开始角度、圆弧的结束角度、是否逆时针
+          ctx.arc(x, y, x - bW, 0, (360 * Math.PI) / 180, false);
+          ctx.fill();
+        } else {
+          ctx.lineWidth = bW;
+          ctx.beginPath();
+          ctx.strokeStyle = bColor;
+          // 圆心x、圆心y、圆的半径、圆弧的开始角度、圆弧的结束角度、是否逆时针
+          ctx.arc(x, y, x - bW / 2, 0, (360 * Math.PI) / 180, false);
+          ctx.stroke();
+        }
+      }
+      // 矩形 左右两边圆
+      else if (options.type == "rect-circle") {
+        if (this.tool.isNotEmpty(bgColor)) {
+          ctx.beginPath();
+          ctx.fillStyle = bColor;
+          ctx.roundRect(0, 0, w, h, h * 2);
+          ctx.fill();
+
+          ctx.beginPath();
+          ctx.fillStyle = bgColor;
+          ctx.roundRect(bW, bW, w - bW * 2, h - bW * 2, h * 2);
+          ctx.fill();
+        } else {
+          ctx.lineWidth = bW;
+          ctx.beginPath();
+          ctx.strokeStyle = bColor;
+          ctx.roundRect(bW / 2, bW / 2, w - bW, h - bW, h * 2);
+          ctx.stroke();
+        }
+      }
+      // 圆形
+      else if (options.type == "circle") {
+        let x = w / 2;
+        let y = h / 2;
+        if (this.tool.isNotEmpty(bgColor)) {
+          ctx.beginPath();
+          ctx.fillStyle = bColor;
+          // 圆心x、圆心y、圆的半径、圆弧的开始角度、圆弧的结束角度、是否逆时针
+          ctx.arc(x, y, x, 0, (360 * Math.PI) / 180, false);
+          ctx.fill();
+
+          ctx.beginPath();
+          ctx.fillStyle = bgColor;
+          // 圆心x、圆心y、圆的半径、圆弧的开始角度、圆弧的结束角度、是否逆时针
+          ctx.arc(x, y, x - bW, 0, (360 * Math.PI) / 180, false);
+          ctx.fill();
+        } else {
+          ctx.lineWidth = bW;
+          ctx.beginPath();
+          ctx.strokeStyle = bColor;
+          // 圆心x、圆心y、圆的半径、圆弧的开始角度、圆弧的结束角度、是否逆时针
+          ctx.arc(x, y, x - bW / 2, 0, (360 * Math.PI) / 180, false);
+          ctx.stroke();
+        }
+      }
     },
     onShapeEnd() {
       let el = this.drag.el;
@@ -59,6 +278,21 @@ export default {
         this.initPosion();
       }
     },
+    createShapeEl(shape, options) {
+      let el = this.tool.jQuery(
+        `<div class="flowchart-new-shape"><canvas class="flowchart-canvas" width="100%" height="100%"></canvas></div>`
+      );
+      this.tool.jQuery(this.$el).append(el);
+      el.css({
+        width: shape.width,
+        height: shape.height,
+      });
+      let canvas = el.find(".flowchart-canvas");
+      if (shape.draw) {
+        shape.draw(canvas[0], options);
+      }
+      return el;
+    },
     ondrop() {
       if (this.drag.isShape && this.drag.el) {
         let el = this.drag.el;
@@ -66,18 +300,11 @@ export default {
         if (el) {
           el.remove();
         }
-        el = this.tool.jQuery(
-          `<div class="flowchart-new-shape">` + this.drag.shape.code + `</div>`
-        );
-        el.css({
-          width: this.drag.shape.width,
-          height: this.drag.shape.height,
-        });
+        el = this.createShapeEl(this.drag.shape, this.drag.shape);
         el.css({
           left: this.shapeLeft,
           top: this.shapeTop,
         });
-        this.tool.jQuery(this.$el).append(el);
       }
     },
     initPosion(e) {
@@ -99,21 +326,13 @@ export default {
       if (this.drag.isShape) {
         let el = this.drag.el;
         if (!el) {
-          el = this.tool.jQuery(
-            `<div class="flowchart-new-shape">` +
-              this.drag.shape.code +
-              `</div>`
-          );
-          el.css({
-            width: this.drag.shape.width,
-            height: this.drag.shape.height,
-          });
+          el = this.createShapeEl(this.drag.shape, this.drag.shape);
+          this.tool.jQuery(this.$el).append(this.drag.el);
         }
         this.drag.el = el;
         this.containerLeft = this.tool.getElementLeft(this.$el);
         this.containerTop = this.tool.getElementTop(this.$el);
         this.initPosion();
-        this.tool.jQuery(this.$el).append(this.drag.el);
         this.drag.onEnd = this.onShapeEnd;
       }
     },
--- src/component/flowchart/index.js
+++ src/component/flowchart/index.js
@@ -3,28 +3,48 @@ var shapeGroups = []
 var baseGroup = { text: "基础", shapes: [] }
 shapeGroups.push(baseGroup)
 
+let clearCanvas = (canvas) => {
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 baseGroup.shapes.push({
     text: "开始/结束", img: "start.png", width: 100, height: 50,
-    code: `
-<svg xmlns="http://www.csnd.org/2000/svg" version="1.1" height="100%" width="100%">
-    <rect width="100%" height="100%" rx="30%" />
-</svg>
-` })
+    draw(canvas, options) {
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        let x = canvas.width / 2
+        let y = canvas.height / 2
+        // 圆心x、圆心y、圆的半径、圆弧的开始角度、圆弧的结束角度、是否逆时针
+        ctx.arc(x, y, x, 0, 360 * Math.PI / 180, false);
+        ctx.strokeStyle = "#ddd";
+        ctx.stroke();
+    },
+})
 
 baseGroup.shapes.push({
     text: "矩形", img: "rect.png", width: 100, height: 50,
-    code: `
-<svg xmlns="http://www.csnd.org/2000/svg" version="1.1" height="100%" width="100%">
-    <rect width="100%" height="100%" rx="10%" ry="10%"/>
-</svg>
-` })
+    draw(canvas, options) {
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle = "#ddd";
+        ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    },
+})
 baseGroup.shapes.push({
     text: "圆形", img: "circle.png", width: 100, height: 100,
-    code: `
-<svg xmlns="http://www.csnd.org/2000/svg" version="1.1" height="100%" width="100%">
-    <circle cx="50%" cy="50%" r="50%"/>
-</svg>
-` })
+    draw(canvas, options) {
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        let x = canvas.width / 2
+        let y = canvas.height / 2
+        // 圆心x、圆心y、圆的半径、圆弧的开始角度、圆弧的结束角度、是否逆时针
+        ctx.arc(x, y, x, 0, 360 * Math.PI / 180, false);
+        ctx.strokeStyle = "#ddd";
+        ctx.stroke();
+    },
+})
 
 let getParallelogramPolygon = (width, height) => {
 
@@ -33,11 +53,13 @@ let getParallelogramPolygon = (width, height) => {
 
 baseGroup.shapes.push({
     text: "平行四边形", img: "parallelogram.png", width: 100, height: 50,
-    code: `
-<svg xmlns="http://www.csnd.org/2000/svg" version="1.1" height="100%" width="100%">
-    ${getParallelogramPolygon(100, 50)}
-</svg>
-` })
+    draw(canvas, options) {
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle = "green";
+        ctx.strokeRect(0, 0, options.width, options.height);
+    },
+})
 
 let getDiamondPolygon = (width, height) => {
 
@@ -46,11 +68,13 @@ let getDiamondPolygon = (width, height) => {
 
 baseGroup.shapes.push({
     text: "菱形", img: "diamond.png", width: 100, height: 70,
-    code: `
-<svg xmlns="http://www.csnd.org/2000/svg" version="1.1" height="100%" width="100%">
-    ${getDiamondPolygon(100, 70)}
-</svg>
-` })
+    draw(canvas, options) {
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle = "green";
+        ctx.strokeRect(0, 0, options.width, options.height);
+    },
+})
 
 
 export default {
